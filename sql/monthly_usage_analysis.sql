e5298f165281fe476bbace1814d25d3976b49f2f
krishita1911 <krishita1911@gmail.com>
2025-12-27 13:40:11 +0530

    Update monthly_usage_analysis.sql

--- sql/monthly_usage_analysis.sql
+++ sql/monthly_usage_analysis.sql
@@ -6,11 +6,9 @@ SELECT
  FORMAT_DATE("%Y-%m",DATE(started_at)) AS Month,
  member_casual,
  Round(AVG(TIMESTAMP_DIFF(ended_at,started_at,MINUTE)),2) AS avg_trip_duration_minutes,
- MAX(TIMESTAMP_DIFF(ended_at,started_at,MINUTE)) AS max_trip_duration_minutes,
  APPROX_QUANTILES(TIMESTAMP_DIFF(ended_at, started_at, MINUTE),2)[OFFSET(1)] AS median_trip_duration_minutes
-FROM 
- `bike_data.bike_trips`
-GROUP BY 
+FROM `bike_data.bike_trips`
+GROUP BY
  Month,member_casual
-ORDER BY 
+ORDER BY
  Month,member_casual;
